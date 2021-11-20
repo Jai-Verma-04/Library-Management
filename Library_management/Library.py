3f7dc07d68f65842dad1d9195d39516bfcb40b85
Jai Verma <94690866+Jai-Verma-04@users.noreply.github.com>
2021-11-20 14:12:59 +0530

    Add files via upload

---
+++ Library_management/Library.py
@@ -0,0 +1,79 @@
+from subprocess import call
+
+'''Creation of the registration and login interface'''
+
+#TODO Implementation of memory saving (Saving the username and passwords to a csv)
+#TODO User once registered can then login again after once closing the application
+#TODO Adding an option to delete a user from the save file
+
+
+registered_users = {}       #* Dictionary os user and password
+
+def register():         #* Function for registering a librarian
+
+    print()
+    
+    user_name = input("Choose a nusername: ")       #* Entering the username for the librarian
+    password = input("Choose a password: ")     #* Choosing a password for thr librarian
+    
+    print(f"{user_name} registered successfully..")     #* Confirms the registration
+    
+    registered_users[user_name]=password        #* Adds username as key and password as value in the dict
+
+
+
+def login():        #* Function for logging in
+    print()
+    
+    user = input("Enter your username: ")       #* Input the username   
+    passwd = input("Enter the password: ")      #* Input the password
+    
+    if user in registered_users.keys():             #* If username is registered
+        
+        if registered_users.get(user) == passwd:    #* If password is also correct  
+            print("Logged in successfully")
+            call(["python", "Library_management\\lib_main.py"])
+            return
+        
+        else:                                       #* If username is correct but password incorrect
+            print("Wrong Passsword!!")
+            main()
+    
+    else:                               #* Username not found the dictionary
+        print("User not found!!")
+        main()
+
+
+def display_librarians():              #* Show the librarians who can access the library
+    
+    for user in registered_users.keys():    #* Fetch username from the dict
+        print(user)
+
+
+if __name__=='__main__':        
+    
+    def main():         #* Interface for the library
+    
+        while True:
+
+            print()
+            print("Welcome to the Library...")
+            print("1. Register librarian")
+            print("2. Login to library")
+            print("3. Show librarians")
+            
+            choose = int(input("Enter a choice: "))    
+
+            if choose==1:
+                register()      #* Calling the register function
+            
+            elif choose==2:
+                login()         #* calling the login function
+            
+            elif choose==3:
+                display_librarians()    #* Calling the display librarian function
+            
+            else:
+                print("wrong choice")
+                main()
+    main()
---
+++ Library_management/lib_main.py
@@ -0,0 +1,96 @@
+
+#TODO Make a saving system in a file (CSV Most probably)
+#TODO Program can retain data even after closed
+
+
+
+class Library:      #* Defining the library class
+    
+    def __init__(self, list, name):     #* Initializing the variables for the class
+        
+        self.booksList = list
+        self.name = name
+        self.lendDict = {}
+        
+    
+    def displaybooks(self):     #* Display the list of books in the library
+    
+        print("\nFolllowing books are there in the", self.name, "Library")
+    
+        for book in self.booksList:     
+    
+            if book not in self.lendDict.keys():    #* If book has not been lent
+                print(book)
+    
+            elif book in self.lendDict.keys():      #* If book has been lent by somenone
+                print(book,"- already taken by",self.lendDict.get(book))
+
+    
+    def lendBook(self, user, book):         #* Lend book to the user
+        
+        if book in self.booksList:      #* Check if book is there in the library
+        
+            if book not in self.lendDict.keys():   #* Check if book has not already been lent
+                self.lendDict.update({book:user})
+                print("\nBook dictionary has been upadated")
+        
+            else:       #* If book already lent
+                print("\nBook already taken by ", self.lendDict[book])
+        
+        else:       #* If book isnt in the library
+            print("\nBook not in Library")
+    
+    
+    def addBook(self, book):        #* Add books to library
+        
+        self.booksList.append(book)     #* Append books to booksList
+        print('\nBook has been added to library!')
+
+    def returnBook(self, book):     #* Return book to library
+        self.lendDict.pop(book)
+        print(book, "returned")
+
+
+if __name__ == '__main__':
+    
+    jai = Library(["Python", "C++", "Java", "DS", "Ruby"], "Jai")   #*Books in library, name of library
+    
+    while True:
+
+        print("\nWelcome to ", jai.name, "library.")
+        print("1. Display")
+        print("2. Lend")
+        print("3. Add")
+        print("4. Return\n")
+        
+        user_choice = int(input("Enter your choice: "))
+
+        if user_choice == 1:    
+            jai.displaybooks()
+
+        elif user_choice == 2:
+            name = input("\nEnter your name: ")
+            book = input("Enter the name of the book you want to lend: ")
+            jai.lendBook(name, book)
+
+        elif user_choice == 3:
+            book = input("\nEnter the name of the book you want to add: ")
+            jai.addBook(book)
+
+        elif user_choice == 4:
+            book = input("\nEnter the name of the book you want to return: ")
+            jai.returnBook(book)
+        
+        else:
+            print("\nNot Valid option")
+            continue
+        
+        print("\nPress q to quit and c to continue\n")
+        
+        user_ch = input().lower()
+        
+        if user_ch == 'q':
+            exit()
+        
+        if user_ch=='c':
+            continue
